59fa4741d856db7d146e27d2da5352b3bb12a377
LeticiaGondo <legondo@outlook.com>
2025-11-10 21:18:18 -0300

    innit

---
+++ src/main/java/br/com/fiap/course/TcFase4Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcFase4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcFase4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/course/TcFase4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcFase4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
